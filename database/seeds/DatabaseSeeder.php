fc6c4b7b785ce4473f6792a2b25692ba91c6ba0c
ogasawarasatoru <world.wide.web.13@gmail.com>
2021-11-22 07:34:51 +0900

    test:OptionTableのseeder作成

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
         // $this->call(UsersTableSeeder::class);
         $this->call(QuestionCategoryTableSeeder::class);
         $this->call(QuestionTableSeeder::class);
-        $this->call(HintTableSeeder::class);
+        $this->call(HintTableSeeder::class);        
+        $this->call(OptionTableSeeder::class);        
     }
 }
