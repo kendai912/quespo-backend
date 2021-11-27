c6ffdf7f12ccbd9742c3e3a138144cbf237805a6
ogasawarasatoru <world.wide.web.13@gmail.com>
2021-11-27 22:46:14 +0900

    feat:tableリレーション

--- app/Models/Option.php
+++ app/Models/Option.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Option extends Model
 {
-    //
+    // userテーブルとの中間テーブル
+    public function users()
+    {
+        return $this->belongsToMany('App\Models\User')->withTimestamps();
+    }
+    // questionテーブルの従テーブル
+    public function question()
+    {
+        return $this->belongsTo('App\Models\Question');
+    }
 }
--- app/Models/QuestionCategory.php
+++ app/Models/QuestionCategory.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class QuestionCategory extends Model
 {
-    //
+
+    // スコープ定義
+    public function scopeDefaultSelect($query)
+    {
+        return $query->addSelect(['id','title','body','img_file_name as img_file_name_question_category','created_at']);
+    }
+
+    // QuestionAreaテーブルと1対1
+    public function questionArea(){
+        return $this->hasOne('App\Models\QuestionArea')->indexAreaSelect();
+    }
+
+    // Questionテーブルの主テーブル
+    public function questions(){
+        return $this->hasMany('App\Models\Question');
+    }
+
 }
