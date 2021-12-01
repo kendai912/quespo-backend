9e4e1f5a6acf6fe1dab29b98fff1e2855f421b1d
ogasawarasatoru <world.wide.web.13@gmail.com>
2021-12-02 05:32:14 +0900

    fix:CORS対応修正--6

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,7 +16,7 @@ class Cors
     public function handle($request, Closure $next)
     {
         return $next($request)
-        ->header('Access-Control-Allow-Origin', 'https://quespo-frontend.herokuapp.com/')
+        ->header('Access-Control-Allow-Origin', 'https://quespo-frontend.herokuapp.com')
         ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
         ->header('Access-Control-Allow-Headers', 'Origin,Authorization,X-Requested-With, Accept, Content-Type');
     }
