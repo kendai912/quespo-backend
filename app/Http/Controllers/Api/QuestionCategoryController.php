d3d965b6e51c360ada5f3d45554bdcc5515b13e2
Kendai <kendai912@gmail.com>
2021-12-05 11:34:01 +0900

    fix:/api/questioncategoriesのバグ修正

--- app/Http/Controllers/Api/QuestionCategoryController.php
+++ app/Http/Controllers/Api/QuestionCategoryController.php
@@ -19,42 +19,40 @@ class QuestionCategoryController extends Controller
      */
 
     public function index()
-    {        
+    {
         $questionCategory = QuestionCategory::with('questionArea')->defaultSelect()->get();
         $complete_flag = null;
         $numOfQuestion = null;
         // ログインユーザーID(1) ※Postmanを使う際はここを数字(任意のuserid)に置き換える
         $uid = Auth::id();
-        //クエスチョンカテゴリーのID一覧(2) 
+        //クエスチョンカテゴリーのID一覧(2)
         $questionCategoryIds = QuestionCategory::pluck('id');
         // (1)(2)を引数に各カテゴリーのユーザーの回答レコードを取得
-        foreach($questionCategoryIds as $i => $cid){
+        foreach ($questionCategoryIds as $i => $cid) {
             // 'option_user','options','questionsno'の３テーブルを繋げてレコード取得
-            $user_ans = Option::whereHas('users', function ($q) use ($uid){
-                $q->where('user_id', $uid)->where(function($custom_q){
-                    $custom_q->where('status','true')->orWhere('status','false_2');
+            $user_ans = Option::whereHas('users', function ($q) use ($uid) {
+                $q->where('user_id', $uid)->where(function ($custom_q) {
+                    $custom_q->where('status', 'true')->orWhere('status', 'false_2');
                 });
-            })->whereHas('question',function($q) use($cid) {
+            })->whereHas('question', function ($q) use ($cid) {
                 $q->where('question_category_id', $cid);
             })->count();
 
-            $numOfQuestion = Question::whereHas('questionCategory', function($q) use ($i){
-                $q->where('id',$i+1);
-            })->count();
+            $numOfQuestion = Question::where('question_category_id', $cid)->count();
 
             // ５問回答済みの場合は回答フラグをtrue/それ以外はfalse
-            if($user_ans === $numOfQuestion){
+            if ($user_ans === $numOfQuestion) {
                 $complete_flag = true;
-                // $questionCategoryに連想配列形式で'complete_flag'を追加
+            // $questionCategoryに連想配列形式で'complete_flag'を追加
             } else {
-                $complete_flag = false;  
+                $complete_flag = false;
             }
             $questionCategory[$i]['complete_flag'] = $complete_flag;
             $questionCategory[$i]['num_of_question'] = $numOfQuestion;
         }
         return response()->json([
             'questionCategories' => json_decode($questionCategory, true)
-        ],200);
+        ], 200);
     }
 
     /**
@@ -86,16 +84,16 @@ class QuestionCategoryController extends Controller
      */
     public function show($questionCategory)
     {
-        $queryCategory = QuestionCategory::where('id',$questionCategory)->With('questionArea')->defaultSelect()->get();
+        $queryCategory = QuestionCategory::where('id', $questionCategory)->With('questionArea')->defaultSelect()->get();
        
         // (1)(2)を引数に[問題:[選択肢:val]]形式のレコード作成
         // questions,hints,optionsテーブルからレコード取得
-        $queryQuestionOptions = Question::with('hint')->with('options')->where('question_category_id',$questionCategory)->defaultSelect()->get();
+        $queryQuestionOptions = Question::with('hint')->with('options')->where('question_category_id', $questionCategory)->defaultSelect()->get();
 
         return response()->json([
             'questionCategory' => json_decode($queryCategory, true),
             'questions' => json_decode($queryQuestionOptions, true)
-        ],200);
+        ], 200);
     }
 
     /**
